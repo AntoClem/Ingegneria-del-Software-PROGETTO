f2387e8d653850a3f28a8cf60c2f97f7ebc51567
Giovanni Aliperta <giova.aliperta@studenti.unina.it>
2026-07-13 14:57:10 +0200

    Delete JavaProject directory
    elimino vecchio javaproject sviluppato per inserire quello corretto

--- JavaProject/src/main/java/it/unina/materialedidattico/database/NotificaDAO.java
+++
@@ -1,8 +0,0 @@
-package it.unina.materialedidattico.database;
-
-import it.unina.materialedidattico.entity.Notifica;
-
-/** Interfaccia DAO per la persistenza dell'Entity Notifica. */
-public interface NotificaDAO {
-    void salva(Notifica notifica);
-}
